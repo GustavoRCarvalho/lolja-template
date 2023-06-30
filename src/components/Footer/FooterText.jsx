b02b46597cc2a1c0a5575af41d9c9b0ffeef0408
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-06-30 02:20:43 -0300

    add: default font-size

--- src/components/Footer/FooterText.jsx
+++ src/components/Footer/FooterText.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const FooterText = styled.div`
   color: ${(props) => props.color ?? "#dddddd"};
-  font-size: 0.8em;
+  font-size: 0.9em;
   padding: 0.2em 0 0.2em 0;
   display: block;
 
