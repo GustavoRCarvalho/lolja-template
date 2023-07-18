73de3449c10faf78a5c18429950b4512cbfa3d03
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-07-17 21:40:29 -0300

    fix: scroll to top every time click on link comp

--- src/components/common/NoStyleLinkRouter.jsx
+++ src/components/common/NoStyleLinkRouter.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 
-export const NoStyleLinkRouter = styled(Link)`
+export const NoStyleLinkRouter = (props) => {
+  window.scrollTo(0, 0)
+  return <NoStyleLink {...props}>{props.children}</NoStyleLink>
+}
+
+const NoStyleLink = styled(Link)`
   text-decoration: none;
   color: black;
 `
